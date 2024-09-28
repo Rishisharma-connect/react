96b2b684ba0d1b5cf5c2aa8e5273682350eb8ce7
Rishi Sharma <contact@natter.org.uk>
2024-09-28 13:08:16 +0100

    git base

---
+++ learn-ssr/src/app/git/page.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const page = () => {
+  return (
+    <div>
+      <h1>This is a server component</h1>
+      <p className="bg-zinc-100">
+        This is a server component, which means it is rendered on the server
+        side.
+      </p>
+    </div>
+  );
+};
+
+export default page;
