7682b576121a6788888ac6ac4e458f843466c14a
Rishi Sharma <contact@natter.org.uk>
2024-09-29 09:42:37 +0100

    changes

--- learn-ssr/src/app/git/page.tsx
+++ learn-ssr/src/app/git/page.tsx
@@ -6,7 +6,7 @@ const page = () => {
       <h1>This is a server component</h1>
       <p className="bg-zinc-[150]">
         This is a server component, which means it is rendered on the server
-        side !!!!.
+        side !!!!!!!!.
       </p>
     </div>
   );
